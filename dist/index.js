1fd4df09786460ef2e51401ec2dc2b6eaf54bde5
AndreevAA <58621780+AndreevAA@users.noreply.github.com>
2022-11-21 11:21:54 +0300

    update

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var app = express();
-app.use('/', function (req, res) { return res.send("dsfdHello world!"); });
-app.use('/login', function (req, res) { return res.send("DLSLKD!"); });
-app.listen(3000, function () { return console.log("Listening"); });
-//# sourceMappingURL=index.js.map
--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express = require("express");
-var app = express();
-app.use('/', function (req, res) { return res.send("Hello world!"); });
-app.listen(3000, function () { return console.log("Listening"); });
-//# sourceMappingURL=index.js.map
